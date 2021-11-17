95275efd76e0e931396f9009b569ca2b48df21a6
S-Toky <stokyr29@gmail.com>
2021-11-17 20:30:07 +0300

    add and init app

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  devise_for :users
+  resources :users, only: [:index, :show]
+  resources :users do
+    resources :relationships, only: [:create]
+  end
+  resources :relationships, only: [:destroy]
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
